b4666e9fdb0c36716bc79bc97265db8142a60d3b
Nathan Rajlich <nathan@tootallnate.net>
2020-06-22 13:39:26 -0700

    Update default Deno version to v1.1.1

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ import {
 
 const { stat, readdir, readFile, writeFile } = fs.promises;
 
-const DEFAULT_DENO_VERSION = 'v1.1.0';
+const DEFAULT_DENO_VERSION = 'v1.1.1';
 
 interface Graph {
 	deps: string[];
