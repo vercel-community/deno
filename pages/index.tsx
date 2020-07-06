ab7a94f97f362b438cb2fe05a0db174e9e574067
Nathan Rajlich <n@n8.io>
2020-07-06 14:24:43 -0700

    Add example URLs to frontend (#20)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,30 @@
-export default () => {
-	return <div>Hello Deno</div>;
+import fs from 'fs';
+import Link from 'next/link';
+import { basename, extname, join } from 'path';
+
+export async function getStaticProps() {
+	const apiDir = join(process.cwd(), 'api');
+	const apiFiles = await fs.promises.readdir(apiDir);
+	const examples = apiFiles.map((f) => basename(f, extname(f)));
+	return { props: { examples } };
+}
+
+export default ({ examples }) => {
+	return (
+		<div>
+			<p>Hello Deno, powered by Vercel!</p>
+			<div>
+				<h3>Examples:</h3>
+				<ul>
+					{examples.map((example) => (
+						<li>
+							<Link href={`/api/${example}`}>
+								<a>{example}</a>
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
