b78baac9fec4b1cf2f52b48af74a9fb4ff664a88
Nathan Rajlich <n@n8.io>
2020-10-08 14:29:47 -0700

    0.7.2

---
+++ api/date.ts
@@ -0,0 +1,7 @@
+import { ServerRequest } from 'https://deno.land/std@0.74.0/http/server.ts';
+
+export default async (req: ServerRequest) => {
+	const body = new Date().toISOString();
+	console.log({ body });
+	req.respond({ body });
+};
