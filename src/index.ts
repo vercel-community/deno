24251654d849b69690753a543b11b8430020bf66
Nathan Rajlich <n@n8.io>
2021-03-05 14:20:07 -0800

    Default to Deno v1.8.0 and `std@0.89.0`

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ interface BuildInfo {
 }
 
 const TMP = tmpdir();
-const DEFAULT_DENO_VERSION = 'v1.5.4';
+const DEFAULT_DENO_VERSION = 'v1.8.0';
 
 // `chmod()` is required for usage with `vercel-dev-runtime` since
 // file mode is not preserved in Vercel deployments from the CLI.
