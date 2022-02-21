e9246d17f71890c5e5d9c8048a61ff9419821b27
Kid <44045911+kidonng@users.noreply.github.com>
2022-02-22 02:50:46 +0800

    Update Deno std used by dev server

--- src/runtime/dev-server.ts
+++ src/runtime/dev-server.ts
@@ -1,7 +1,8 @@
-import { Response, serve } from 'https://deno.land/std@0.106.0/http/server.ts';
+import { serve } from 'https://deno.land/std@0.126.0/http/server.ts';
+import { writeAllSync } from 'https://deno.land/std@0.126.0/streams/conversion.ts';
 
-function isNetAddr(v: any): v is Deno.NetAddr {
-	return v && typeof v.port === 'number';
+function random(min: number, max: number): number {
+	return Math.round(Math.random() * (max - min)) + min;
 }
 
 // Load the entrypoint handler function
@@ -15,34 +16,35 @@ if (typeof handler !== 'function') {
 }
 
 // Spawn HTTP server on ephemeral port
-const s = serve({ hostname: '127.0.0.1', port: 0 });
+let port: number;
 
-if (isNetAddr(s.listener.addr)) {
-	const { port } = s.listener.addr;
-	const portBytes = new TextEncoder().encode(String(port));
-
+while (true) {
+	port = random(10000, 65535);
+	
 	try {
-		// Write the port number to FD 3
-		const portFd = Deno.openSync('/dev/fd/3', { read: false, write: true });
-		Deno.writeAllSync(portFd, portBytes);
-		Deno.close(portFd.rid);
+		serve(handler, { hostname: '127.0.0.1', port });
+		break
 	} catch (err) {
-		// This fallback is necessary for Windows
-		// See: https://github.com/denoland/deno/issues/6305
-		const portFile = Deno.env.get('VERCEL_DEV_PORT_FILE');
-		if (portFile) {
-			await Deno.writeFile(portFile, portBytes);
+		if (!(err instanceof Deno.errors.AddrInUse)) {
+			throw err;
 		}
-	} finally {
-		Deno.env.delete('VERCEL_DEV_PORT_FILE');
 	}
 }
 
-// Serve HTTP requests to handler function
-for await (const req of s) {
-	Promise.resolve(handler(req)).then((res: Response | void) => {
-		if (res) {
-			return req.respond(res);
-		}
-	});
+const portBytes = new TextEncoder().encode(String(port));
+
+try {
+	// Write the port number to FD 3
+	const portFd = Deno.openSync('/dev/fd/3', { read: false, write: true });
+	writeAllSync(portFd, portBytes);
+	Deno.close(portFd.rid);
+} catch (err) {
+	// This fallback is necessary for Windows
+	// See: https://github.com/denoland/deno/issues/6305
+	const portFile = Deno.env.get('VERCEL_DEV_PORT_FILE');
+	if (portFile) {
+		await Deno.writeFile(portFile, portBytes);
+	}
+} finally {
+	Deno.env.delete('VERCEL_DEV_PORT_FILE');
 }
