403dc715628e5ec9c8926990118a53aae53f182a
Nathan Rajlich <n@n8.io>
2022-03-21 13:40:28 -0700

    Use Git sha instead of "master" for the Source link (#109)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import Link from 'next/link';
 import { basename, extname, join } from 'path';
 
 export async function getStaticProps() {
+	const sha = process.env.VERCEL_GIT_COMMIT_SHA || 'master';
 	const apiDir = join(process.cwd(), 'api');
 	const apiFiles = await fs.promises.readdir(apiDir);
 	const examples = apiFiles
 		.filter((f) => f.endsWith('.ts') || f.endsWith('.js'));
-	return { props: { examples } };
+	return { props: { sha, examples } };
 }
 
-export default function Index ({ examples }) {
+export default function Index ({ sha, examples }) {
 	return (
 		<div>
 			<p>Hello from Deno, powered by Vercel!</p>
@@ -24,7 +25,7 @@ export default function Index ({ examples }) {
 							</Link>
 							{' '}
 							(
-								<Link href={`https://github.com/vercel-community/deno/blob/master/api/${example}`}>
+								<Link href={`https://github.com/vercel-community/deno/blob/${sha}/api/${example}`}>
 									<a target="_blank" rel="noopener noreferrer">Source</a>
 								</Link>
 							)
