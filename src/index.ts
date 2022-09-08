2221c22ab432d3e6ed955812477b5cf769d5c924
Nathan Rajlich <n@n8.io>
2022-09-08 10:51:49 -0700

    v1.25.1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * The default version of Deno that will be downloaded at build-time.
  */
-const DEFAULT_DENO_VERSION = 'v1.25.0';
+const DEFAULT_DENO_VERSION = 'v1.25.1';
 
 import { fileURLToPath, pathToFileURL } from 'url';
 import { spawn } from 'child_process';
