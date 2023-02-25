d8184dfbf505786ce7f58247a654647def24c8d4
Nathan Rajlich <n@n8.io>
2023-02-25 00:26:51 -0800

    Default to Deno v1.31.1 (#137)

--- src/deno-lambda.ts
+++ src/deno-lambda.ts
@@ -1,7 +1,7 @@
 /**
  * The default version of Deno that will be downloaded at build-time.
  */
-const DEFAULT_DENO_VERSION = 'v1.30.3';
+const DEFAULT_DENO_VERSION = 'v1.31.1';
 
 import { spawn } from 'child_process';
 import { fileURLToPath, pathToFileURL } from 'url';
@@ -164,6 +164,12 @@ export class DenoLambda extends Lambda {
 				cwd,
 				join(cwd, entrypoint)
 			),
+			glob('node_modules/.deno/**', {
+				cwd,
+				includeDirectories: true,
+			}).then((files) => {
+				Object.assign(outputFiles, files);
+			}),
 		]);
 
 		// Add additional files that were referenced from
