7a6e070911bc67723d1eb84428d8638f1fa88817
Nathan Rajlich <n@n8.io>
2023-04-04 20:52:35 -0700

    Default to Deno v1.32.3 (#140)

--- src/deno-lambda.ts
+++ src/deno-lambda.ts
@@ -1,7 +1,7 @@
 /**
  * The default version of Deno that will be downloaded at build-time.
  */
-const DEFAULT_DENO_VERSION = 'v1.32.0';
+const DEFAULT_DENO_VERSION = 'v1.32.3';
 
 import { spawn } from 'child_process';
 import { fileURLToPath, pathToFileURL } from 'url';
